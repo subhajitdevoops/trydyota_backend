ca68f9c575c67d1d86b00a9c997f2775d0090ef7
subhajitdevoops <subhajit@codermaniaa.com>
2023-10-11 14:45:00 +0530

    modify

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path");
+
 
 const { connectDB } = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
