a1e92a7c3bc4e92733b275137381dcf08e572627
subhajitdevoops <121277474+subhajitdevoops@users.noreply.github.com>
2023-10-11 21:11:30 +0530

    Add files via upload
    1

--- api/index.js
+++ api/index.js
@@ -31,8 +31,9 @@ app.use(express.json({ limit: "4mb" }));
 app.use(helmet());
 app.use(cors());
 
-
+//root route
 app.get("/", (req, res) => {
+  console.log("jjj");
   res.send("App works properly!");
 });
 
@@ -46,25 +47,41 @@ app.use("/api/attributes/", attributeRoutes);
 app.use("/api/setting/", settingRoutes);
 app.use("/api/currency/", isAuth, currencyRoutes);
 app.use("/api/language/", languageRoutes);
-console.log("2");
 
 //if you not use admin dashboard then these two route will not needed.
 app.use("/api/admin/", adminRoutes);
 app.use("/api/orders/", orderRoutes);
-console.log("3");
 
+// app.get("/", async (req, res, next) => {
+//   try {
+//     let html = fs.readFileSync(path.resolve(root, "index.html"), "utf-8");
+
+//     // Transform HTML using Vite plugins.
+//     html = await viteServer.transformIndexHtml(req.url, html);
+
+//     res.send(html);
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
 
 // Use express's default error handling middleware
 app.use((err, req, res, next) => {
   if (res.headersSent) return next(err);
   res.status(400).json({ message: err.message });
 });
-console.log("5");
 
+// Serve static files from the "dist" directory
+// app.use(express.static(path.join(__dirname, "build")));
+app.use("/static", express.static("public"));
 
+// Serve the index.html file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
-console.log("6");
+
 // app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));  
