4323377c11e199fddffbaf7b1540b3b623e43ca4
subhajitdevoops <subhajit@codermaniaa.com>
2023-10-19 17:47:54 +0530

    tax modify

--- controller/taxController.js
+++ controller/taxController.js
@@ -4,11 +4,74 @@ const Tax = require("../models/tax");
 
 const getTax = async (req, res) => {
     try {
-      const taxDetails = await Tax.find();
+
+            var data=req.query;
+
+            var prevPage;                                        
+            var nextPage;
+            var hasPrevPage;
+            var hasNextPage;
+
+            var limit = data.limit ? Number(data.limit) : 10;
+            var page = data.page ? Number(data.page) : 1;
+
+            const taxDetails =await Tax.find().skip((page-1)*limit).limit(Number(limit)).exec();
+            
+
+            var countdata=await Tax.find().exec();
+
+            var count=countdata.length;
+
+            var totallength=Math.ceil(count/limit);
+
+            if(totallength==1 && page==totallength ){
+                prevPage=null;
+                hasPrevPage=false;
+                nextPage=null;
+                hasNextPage=false;
+                
+            }
+            else if(page==1 && totallength>page) {
+                        prevPage=null;
+                        hasPrevPage=false;``
+                        nextPage=Number(page)+1; 
+                        hasNextPage=true;
+                        
+            }
+            else if(page>1 && page==totallength){
+                    prevPage=Number(page)-1;
+                    hasPrevPage=true;
+                    nextPage=null;
+                    hasNextPage=false;
+                    
+            }
+            else{
+                    prevPage=Number(page)-1;
+                    nextPage=Number(page)+1;
+                    hasPrevPage=true;
+                    hasNextPage=true;
+                    
+            }
+
+            const  Pagination ={
+              "TotalDocuments":count,
+              "limit":limit,
+              "TotalPages":totallength,
+              "Current Page":page,
+              "PrevPage":prevPage,
+              "NextPage":nextPage,
+              "HasPrevPage":hasPrevPage,
+              "HasNextPage":hasNextPage,
+              "PagingCounter":page,        // consider index starting from 1,so pagingcounter will be same like index number //
+              
+              }
+
+      
       res.status(200).send({
         success:true,
         message:"Sucessfully fetch!",
-        taxDetails
+        taxDetails,
+        Pagination
       })
     } catch (err) {
         console.log(err),
