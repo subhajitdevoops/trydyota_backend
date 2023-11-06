71fa85c77d420c2055b207c4700a0fd2a5430f3b
subhajitdevoops <subhajit@codermaniaa.com>
2023-11-06 18:17:47 +0530

    index merged and notification file create

--- api/index.js
+++ api/index.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const path = require("path");
-
+const socket = require("socket.io");
 
 const { connectDB } = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
@@ -17,33 +16,28 @@ const attributeRoutes = require("../routes/attributeRoutes");
 const settingRoutes = require("../routes/settingRoutes");
 const currencyRoutes = require("../routes/currencyRoutes");
 const languageRoutes = require("../routes/languageRoutes");
-const taxRoutes = require("../routes/taxRoutes");
-const homepageRoutes = require("../routes/homepageRoutes");
-const shippingRoutes = require("../routes/shippingRoutes");
-const brandCatalogRoutes = require("../routes/brandCatalogRoute");
-const productRequestRoutes=require("../routes/productRequestRoutes");
-
 const { isAuth, isAdmin } = require("../config/auth");
+const {
+  getGlobalSetting,
+  getStoreCustomizationSetting,
+} = require("../lib/notification/setting");
 
 connectDB();
 const app = express();
 
 // We are using this for the express-rate-limit middleware
-// See: https://github.com/nfriedly/express-rate-limit
-// app.enable('trust proxy');
 app.set("trust proxy", 1);
 
 app.use(express.json({ limit: "4mb" }));
 app.use(helmet());
 app.use(cors());
 
-//root route
+// Root route
 app.get("/", (req, res) => {
-  console.log("jjj");
   res.send("App works properly!");
 });
 
-//this for route will need for store front, also for admin dashboard
+// Routes for store front and admin dashboard
 app.use("/api/products/", productRoutes);
 app.use("/api/category/", categoryRoutes);
 app.use("/api/coupon/", couponRoutes);
@@ -53,48 +47,49 @@ app.use("/api/attributes/", attributeRoutes);
 app.use("/api/setting/", settingRoutes);
 app.use("/api/currency/", isAuth, currencyRoutes);
 app.use("/api/language/", languageRoutes);
-app.use("/api/tax/", taxRoutes);
-app.use("/api/homepage/", homepageRoutes);
-app.use("/api/shipping/", shippingRoutes);
-app.use("/api/brandcatalog/", brandCatalogRoutes);
-app.use("/api/productRequest/", productRequestRoutes);
 
-
-
-//if you not use admin dashboard then these two route will not needed.
+// Routes for admin dashboard
 app.use("/api/admin/", adminRoutes);
 app.use("/api/orders/", orderRoutes);
 
-app.get("/", async (req, res, next) => {
-  try {
-    let html = fs.readFileSync(path.resolve(root, "index.html"), "utf-8");
-
-    // Transform HTML using Vite plugins.
-    html = await viteServer.transformIndexHtml(req.url, html);
-
-    res.send(html);
-  } catch (e) {
-    return next(e);
-  }
-});
-
-// Use express's default error handling middleware
-app.use((err, req, res, next) => {
-  if (res.headersSent) return next(err);
-  res.status(400).json({ message: err.message });
-});
-
-// Serve static files from the "dist" directory
-// app.use(express.static(path.join(__dirname, "build")));
-app.use("/static", express.static("public"));
-
-// Serve the index.html file for all routes
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
-
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));  
+// Set up socket
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["PUT", "GET", "POST", "DELETE", "PATCH", "OPTIONS"],
+    credentials: false,
+    transports: ["websocket", "polling"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("notification", async (data) => {
+    let updatedData = data;
+
+    if (data?.option === "storeCustomizationSetting") {
+      const storeCustomizationSetting = await getStoreCustomizationSetting(data);
+      updatedData = {
+        ...data,
+        storeCustomizationSetting: storeCustomizationSetting,
+      };
+    }
+    if (data?.option === "globalSetting") {
+      const globalSetting = await getGlobalSetting(data);
+      updatedData = {
+        ...data,
+        globalSetting: globalSetting,
+      };
+    }
+    io.emit("notification", updatedData);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Socket ${socket.id} disconnected!`);
+  });
+});
