e28601e2d4f3dc7952f4c2b005ae2b5d56a4ca59
subhajitdevoops <subhajit@codermaniaa.com>
2023-11-07 11:47:56 +0530

    server const added

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path");
 const socket = require("socket.io");
 
 const { connectDB } = require("../config/db");
@@ -20,12 +21,9 @@ const taxRoutes = require("../routes/taxRoutes");
 const homepageRoutes = require("../routes/homepageRoutes");
 const shippingRoutes = require("../routes/shippingRoutes");
 const brandCatalogRoutes = require("../routes/brandCatalogRoute");
-const productRequestRoutes=require("../routes/productRequestRoutes");
+const productRequestRoutes = require("../routes/productRequestRoutes");
+
 const { isAuth, isAdmin } = require("../config/auth");
-const {
-  getGlobalSetting,
-  getStoreCustomizationSetting,
-} = require("../lib/notification/setting");
 
 connectDB();
 const app = express();
@@ -39,10 +37,11 @@ app.use(cors());
 
 // Root route
 app.get("/", (req, res) => {
+  console.log("jjj");
   res.send("App works properly!");
 });
 
-// Routes for store front and admin dashboard
+// Routes needed for store front and admin dashboard
 app.use("/api/products/", productRoutes);
 app.use("/api/category/", categoryRoutes);
 app.use("/api/coupon/", couponRoutes);
@@ -50,7 +49,7 @@ app.use("/api/customer/", customerRoutes);
 app.use("/api/order/", isAuth, customerOrderRoutes);
 app.use("/api/attributes/", attributeRoutes);
 app.use("/api/setting/", settingRoutes);
-app.use("/api/currency/",currencyRoutes);
+app.use("/api/currency/", currencyRoutes);
 app.use("/api/language/", languageRoutes);
 app.use("/api/tax/", taxRoutes);
 app.use("/api/homepage/", homepageRoutes);
@@ -62,11 +61,33 @@ app.use("/api/productRequest/", productRequestRoutes);
 app.use("/api/admin/", adminRoutes);
 app.use("/api/orders/", orderRoutes);
 
+app.get("/", async (req, res, next) => {
+  try {
+    let html = fs.readFileSync(path.resolve(root, "index.html"), "utf-8");
+
+    // Transform HTML using Vite plugins.
+    html = await viteServer.transformIndexHtml(req.url, html);
+
+    res.send(html);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err);
+  res.status(400).json({ message: err.message });
+});
+
+// Serve static files from the "dist" directory
+app.use("/static", express.static("public"));
+
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+
+const server = app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
 
 // Set up socket
 const io = socket(server, {
@@ -76,10 +97,15 @@ const io = socket(server, {
     credentials: false,
     transports: ["websocket", "polling"],
   },
+  // allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
+  // console.log(`Socket ${socket.id} connected!`);
+
   socket.on("notification", async (data) => {
+    // console.log("data", data);
+
     let updatedData = data;
 
     if (data?.option === "storeCustomizationSetting") {
