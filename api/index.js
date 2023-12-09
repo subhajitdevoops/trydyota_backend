e3225342941baf6b96ee38506768f565045a2bc4
suhajit@codermaniaa.com <suhajit@codermaniaa.com>
2023-12-09 02:25:35 -0800

    imagekit done

--- api/index.js
+++ api/index.js
@@ -6,6 +6,14 @@ const path = require("path");
 const socket = require("socket.io");
 const bodyParser = require("body-parser");
 
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
+
 
 const { connectDB } = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
@@ -30,7 +38,6 @@ const askForPriceRoutes = require("../routes/askForPriceRoutes");
 const enquiryRoutes = require("../routes/enquiryRoutes.js");
 
 
-
 const { isAuth, isAdmin } = require("../config/auth");
 
 connectDB();
@@ -42,9 +49,19 @@ app.set("trust proxy", 1);
 app.use(express.json({ limit: "4mb" }));
 app.use(helmet());
 
+console.log("1111");
+app.get("/auth", (req, res) => {
+  try {
+    var authenticationParameters = imagekit.getAuthenticationParameters();
+    res.send(authenticationParameters);
+  } catch (err) {
+    console.log("error in auth imagekit!!!!!!!!!!!!!!!");
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // Root route
 app.get("/", (req, res) => {
-  console.log("jjj");
   res.send("App works properly!");
 });
 
@@ -69,9 +86,6 @@ app.use("/api/askforprice/", askForPriceRoutes);
 app.use("/api/enquiry/", enquiryRoutes);
 
 
-
-
-
 // Routes for admin dashboard
 app.use("/api/admin/", adminRoutes);
 app.use("/api/orders/", orderRoutes);
@@ -89,13 +103,22 @@ app.use("/api/orders/", orderRoutes);
 //   }
 // });
 
+
+// //==============For Media Access or Serve static files===============//
+// app.use("/media", express.static(path.join(__dirname, "/upload")));
+
+// app.use("/",express.static(path.join(__dirname, "/public/build")));
+
+//====================== Load the ImageKit Auth Route =======================//
+
+
 app.use((err, req, res, next) => {
   if (res.headersSent) return next(err);
   res.status(400).json({ message: err.message });
 });
 
 // Serve static files from the "dist" directory
-app.use("/static", express.static("public"));
+// app.use("/static", express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 
